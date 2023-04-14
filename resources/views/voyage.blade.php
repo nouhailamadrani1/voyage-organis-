b221dc6760039de32f1f33494bbca0ec3ff3d2b7
nouhailamadrani1 <nouhailamadrani2022@gmail.com>
2023-04-14 10:24:00 +0100

    voyage

--- resources/views/voyage.blade.php
+++ resources/views/voyage.blade.php
@@ -30,35 +30,26 @@
 
         <div class="offcanvas offcanvas-top h-100" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasTopLabel">Offcanvas top</h5>
+            <h5 class="offcanvas-title" id="offcanvasTopLabel">Voyage</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body d-flex justify-content-between flex-wrap  ">
-            <div>     <div> <form>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-              </div>
-              <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-              </div>
+            <div class="w-50  p-2">   <form class="w-100">
+            <h5>réservation</h5>
+
+             
               <button type="submit" class="btn btn-primary">Submit</button>
-            </form></div></div>
-            <div> 
-      <div class="d-flex justify-content-start flex-wrap p-2 w-50" >
+            </form>
+             </div>
+            <div class="w-50 p-2  "> 
+      <div class="d-flex  flex-wrap p-2 w-100 my-2  shadow"  >
       
-            <div class="w-50">  <h5>Titre :{{ $voyage->Titre }}</b></h5>
+            <div class="w-50 p-2">  <h5>Titre :{{ $voyage->Titre }}</b></h5>
         <p>{{ $voyage->description }}</p></div>
         <img src="images/{{ ($voyage->image) }}" class="w-50" alt="..."></div>
-        <div>
+        <div class=" shadow text-start p-2">
           <h6>Transport</h6>
-          <div class="d-flex">
+          <div class="d-flex justify-content-between">
             <div>  <h6 class="my-0"><b>{{$voyage->transport->nom}}</b></p>
                 <p class="fw-light my-0"><b>Nombre passagers:</b>{{$voyage->transport->nombre_passagers}}</p>
                 <p class="fw-light my-0"><b>De puis:</b>{{$voyage->depuis}}</p>
@@ -67,11 +58,11 @@
                 <p class="fw-light my-0"><b>Heure arrivee :</b>{{$voyage->heure_arrivee}}</p>
                 <p class="fw-light my-0"><b>Date départ :</b>{{$voyage->date_de_départ}}</p>
                 <p class="fw-light my-0"><b>Date arrivee :</b>{{$voyage->date_arrivee}}</p></div>
-                <img src="images/{{$voyage->transport->image}}" alt="">
+                <img src="images/{{$voyage->transport->image}}" style="width: 40%" alt="">
           </div>
         </div>
-        <div><div class="d-flex  ">
-          <img src="images/{{$voyage->hotel->image}}" style="width: 20%">
+        <div class=" card my-2 p-2 d-flex justify-content-center border-0 shadow "><div class="d-flex justify-content-between ">
+          <img src="images/{{$voyage->hotel->image}}" style="width: 30%">
  <div class="p-2">  
      <h5 class="card-title">{{$voyage->hotel->nom}}</b><span><img  class="" src="https://img.icons8.com/3d-fluency/20/null/star.png"/>({{$voyage->hotel->nbre_etoiles}}) </span> </h5>
      <p class="fw-light"><b>Ville:</b>{{$voyage->hotel->ville}}<b></p>
